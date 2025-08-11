2bb12429b0851f4bfa015fb752cf5ee5e5accf56
Lok <timothybaca5231@gmail.com>
2025-08-11 16:44:27 +0800

    我的页面

--- lib/pages/my/my_state.dart
+++ lib/pages/my/my_state.dart
@@ -3,13 +3,13 @@ import 'package:get/get.dart';
 
 class MyState extends BaseState with StateAutoManager {
   /// 登录成功状态监听
-  final logined = true.obs; // 设置为已登录状态用于演示
-  
+  final logined = true.obs;
+
   /// 用户信息
   final userName = 'Duo-123'.obs;
   final userId = 'ID:30702607'.obs;
   final userBalance = '19999999.000'.obs;
-  final userAvatar = ''.obs; // 头像URL，可以设置默认头像
+  final userAvatar = ''.obs; // 头像
 
   MyState() {
     /// Initialize variables
